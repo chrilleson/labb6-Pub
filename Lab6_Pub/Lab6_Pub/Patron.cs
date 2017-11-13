ccb30748131c09992b32c290ea15f097accf5cb1
chrilleson <Christoffer.Ljungqvist@hotmail.se>
2017-11-13 13:13:46 +0100

    Created Patrons class

---
+++ Lab6_Pub/Lab6_Pub/Patron.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_Pub
+{
+    class Patron
+    {
+        string Name { get; set; }
+
+        public Patron(string name)
+        {
+            name = Name;
+        }
+    }
+}
