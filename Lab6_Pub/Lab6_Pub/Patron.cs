df8a0e39ca1575c232e4d5e0417c6d2e8c877a3e
chrilleson <Christoffer.Ljungqvist@hotmail.se>
2017-11-21 14:52:23 +0100

    Patron is done

--- Lab6_Pub/Lab6_Pub/Patron.cs
+++ Lab6_Pub/Lab6_Pub/Patron.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Collections.Concurrent;
 
 namespace Lab6_Pub
 {
@@ -13,12 +15,49 @@ namespace Lab6_Pub
         Queue<string> temporaryQueue = new Queue<string>();
 
         private int PatronDrinkTimeMin = 10000;
-        private int PatronDrinkMax = 20000;
+        private int PatronDrinkTimeMax = 20000;
 
         public Patron(string name)
         {
             name = Name;
             NameQueue.Enqueue(Name);
+        }
+
+        private Action<string> Callback;
+        private ConcurrentStack<Chair> EmptyChairStack;
+        private ConcurrentStack<Glass> DirtyGlassStack;
+        private ConcurrentQueue<Patron> PatronQueue;
+        public string PatronDrinking { get; set; }
+        Random rnd = new Random();
+
+        public void Sit(Action<string> Callback, ConcurrentStack<Chair> EmptyChairStack, ConcurrentStack<Glass> DirtyGlassStack, ConcurrentQueue<Patron>PatronQueue, ConcurrentQueue<string> uiPatronCountDeQueue, int speed)
+        {
+            this.Callback = Callback;
+            this.EmptyChairStack = EmptyChairStack;
+            this.DirtyGlassStack = DirtyGlassStack;
+            this.PatronQueue = PatronQueue;
+
+            Task.Run(() =>
+            {
+                temporaryQueue.Enqueue(PatronQueue.FirstOrDefault().Name);
+                PatronDrinking = temporaryQueue.First();
+                temporaryQueue.Dequeue();
+                PatronQueue.TryDequeue(out Patron p);
+
+                while (EmptyChairStack.IsEmpty)
+                {
+                    Callback($"{PatronDrinking} is trying to find a seat.");
+                    Thread.Sleep(1000 / speed);
+                }
+                EmptyChairStack.TryPop(out Chair c);
+                Thread.Sleep(4000 / speed);
+                Callback($"{PatronDrinking} takes a seat.");
+                Thread.Sleep(rnd.Next(PatronDrinkTimeMin / speed, PatronDrinkTimeMax / speed));
+                uiPatronCountDeQueue.TryDequeue(out string s);
+                EmptyChairStack.Push(new Chair());
+                DirtyGlassStack.Push(new Glass());
+                Callback($"{PatronDrinking} is done drinking and leaves the bar.");
+            });
         }
     }
 }
--- Lab6_Pub/Lab6_Pub/Waitress.cs
+++ Lab6_Pub/Lab6_Pub/Waitress.cs
@@ -10,6 +10,7 @@ namespace Lab6_Pub
 {
     public class Waitress
     {
+        /*
         private Action<string> Callback;
         private ConcurrentStack<Glass> DirtyGlassStack;
         private ConcurrentStack<Glass> CleanGlassStack;
@@ -56,6 +57,6 @@ namespace Lab6_Pub
         public void ChangeSpeed(int speed)
         {
             this.waiterSpeed = speed;
-        }
+        }*/
     }
 }
